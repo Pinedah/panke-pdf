d2b84b6af3479cc8f5932446603bb61b9b99549c
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-16 22:26:37 -0600

    Create pdftoimage.py

---
+++ pdftoimage.py
@@ -0,0 +1,14 @@
+
+from pdftoimage import convert_from_path
+
+# Ruta del archivo PDF
+pdf_path = "C:\\Users\\Dell Latitude\\Downloads\\faxihorario.pdf"
+
+# Convertir PDF a imágenes (una imagen por cada página del PDF)
+imagenes = convert_from_path(pdf_path)
+
+# Guardar cada página como una imagen JPG
+for i, imagen in enumerate(imagenes):
+    imagen.save(f'pagina_{i+1}.jpg', 'JPEG')
+
+print("Conversión completada.")
